6cd1fe95550b86f073ba78b7457202ba53ee328d
AilaVarun <vavarunaila@gmail.com>
2025-09-06 22:10:40 -0500

    Add currency variable to .env, enhance HTML structure, implement admin routes, and improve UI components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,11 @@ import Movies from './pages/Movies'
 import MyBookings from './pages/MyBookings'
 import SeatLayout from './pages/SeatLayout'
 import Favorite from './pages/Favorite'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 import { Toaster } from 'react-hot-toast'
 
 const App = () => {
@@ -23,6 +28,12 @@ const App = () => {
       <Route path='/movies/:id/:date' element={<SeatLayout />} />
       <Route path='/my-bookings' element={<MyBookings />} />
       <Route path='/favorite' element={<Favorite />} />
+      <Route path='/admin/*' element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path='add-shows' element={<AddShows />} />
+        <Route path='list-shows' element={<ListShows />} />
+        <Route path='list-bookings' element={<ListBookings />} />
+      </Route>
     </Routes>
     {!isAdminRoute && <Footer/>}  
     </> 
